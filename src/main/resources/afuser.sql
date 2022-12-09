1aa36cdf17701982d6843509e9a05dfd5276c8ef
Belhadi Manssour <33765559+mnsDZ@users.noreply.github.com>
2022-12-09 15:58:57 +0100

    Create afuser.sql
    add database

---
+++ src/main/resources/afuser.sql
@@ -0,0 +1,8 @@
+INSERT INTO af_user (
+id, country, date_of_birth, gender, phone_number, username) VALUES (
+'1', 'FRANCE', '1997-10-10', 'MALE', '05050505', 'USER1')
+
+
+INSERT INTO af_user (
+id, country, date_of_birth, gender, phone_number, username) VALUES (
+'2', 'FRANCE', '1990-10-10', 'FEMALE', '04040404', 'USER2')
